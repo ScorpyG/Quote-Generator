2c024ac8cbfba5bfdc1c87facf2fc5d1624e5bf8
scorpy <justinhoang90@gmail.com>
2024-05-27 19:49:33 -0700

    fix: make get route by id public to avoid issue with auth when getStaticProps executing

--- src/pages/api/quote/get/[qid].ts
+++ src/pages/api/quote/get/[qid].ts
@@ -5,29 +5,27 @@ import { NextApiRequest, NextApiResponse } from 'next';
 async function handler(request: NextApiRequest, response: NextApiResponse) {
   await dbConnect();
 
-  const token = request.headers.authorization?.split(' ')[1];
-  if (!token) {
-    return response.status(401).json({
-      message: 'Unauthorized.',
-    });
-  }
-
   try {
     const quoteId = request.query.qid;
     const quote = await Quote.findById(quoteId);
 
     if (!quote) {
-      return response.status(204).json({ message: 'Quote not found.' });
+      return response.status(204).json({
+        message: 'Quote not found.',
+        success: false,
+      });
     } else {
       return response.status(200).json({
         data: quote,
         message: 'Quote retrieved successfully.',
+        success: true,
       });
     }
   } catch (error) {
     return response.status(500).json({
       error,
       message: 'Unable to retrieve the quote.',
+      success: false,
     });
   }
 }
