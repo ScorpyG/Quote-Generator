c0e9e0b49fe9dcc82a70b1f6afb00db234c5fc94
scorpy <justinhoang90@gmail.com>
2024-07-29 18:48:12 -0700

    chore: add blog schema model

---
+++ src/models/Blog.ts
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+
+export interface TBlog extends Document {
+  userId: mongoose.Schema.Types.ObjectId;
+  title: string;
+  author: string;
+  description: string;
+  imageUrl?: string;
+  tags?: string[];
+}
+
+const BlogSchema = new mongoose.Schema<TBlog>(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+    },
+    tags: [String],
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+const Blog = mongoose.models.Blog || mongoose.model('Blog', BlogSchema, 'blogs');
+
+export default Blog;
